14c1010544a0c4702c7b3326053da70164e8c34a
decent learner <92971853+decentlearner@users.noreply.github.com>
2022-10-01 01:08:08 +0530

    Create two-pointer.cpp
    added C++ code for two pointer approach

---
+++ two-pointer.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool isPairSum(int A[], int N, int X){
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            // as equal i and j means same element
+            if (i == j)
+                continue;
+ 
+            // pair exists
+            if (A[i] + A[j] == X)
+                return true;
+ 
+            // as the array is sorted
+            if (A[i] + A[j] > X)
+                break;
+        }
+    }
+    return false;
+}
+ 
+int main(){
+    int arr[] = { 2, 3, 5, 8, 9, 10, 11 };
+    int val = 17;
+    int arrSize = *(&arr + 1) - arr;
+    sort(arr, arr + arrSize); // Sort the array
+    cout << isPairSum(arr, arrSize, val);
+    return 0;
+}
